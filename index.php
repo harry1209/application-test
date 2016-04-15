04e6f4eb3cdae3839b62df053c21152e166017e2
Pan Xiao <harry1209xiaopan@gmail.com>
2016-04-15 12:16:00 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-Place your code here
+<?php
+  $people = array(
+   array('id'=>1, 'first_name'=>'John', 'last_name'=>'Smith', 'email'=>'john.smith@hotmail.com'),
+   array('id'=>2, 'first_name'=>'Paul', 'last_name'=>'Allen', 'email'=>'paul.allen@microsoft.com'),
+   array('id'=>3, 'first_name'=>'James', 'last_name'=>'Johnston', 'email'=>'james.johnston@gmail.com'),
+   array('id'=>4, 'first_name'=>'Steve', 'last_name'=>'Buscemi', 'email'=>'steve.buscemi@yahoo.com'),
+   array('id'=>5, 'first_name'=>'Doug', 'last_name'=>'Simons', 'email'=>'doug.simons@hotmail.com')
+   );
+?>
+<!doctype html>
+<html>
+   <table cellpadding='5'>
+     <tr>
+       <th>id</th>
+       <th>first_name</th>
+       <th>last_name</th>
+       <th>email</th>
+       <th></th>       
+     </tr>
+     <? foreach ($people as $row) : ?>
+     <tr>
+       <td align="center"><? echo $row["id"]; ?></td>
+       <td align="center"><? echo $row["first_name"]; ?></td>
+       <td align="center"><? echo $row["last_name"]; ?></td>
+       <td align="center"><? echo $row["email"]; ?></td>
+       <td align="center"><? echo "<button onclick='alert(\"". $row['first_name'] . " " . $row['last_name'] . "    " . $row['email'] ."\")'>Click Me</button>"; ?></td>
+     </tr>
+     <? endforeach; ?>
+   </table>
+</html>
